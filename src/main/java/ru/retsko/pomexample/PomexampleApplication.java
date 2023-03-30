2eb0a39433eaeb849c8c348f9889628ef2cb133b
rubikkubika <rubikkubika@gmail.com>
2023-03-30 11:14:14 +0300

    Initial commit

---
+++ src/main/java/ru/retsko/pomexample/PomexampleApplication.java
@@ -0,0 +1,13 @@
+package ru.retsko.pomexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/retsko/pomexample/PomexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.retsko.pomexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
